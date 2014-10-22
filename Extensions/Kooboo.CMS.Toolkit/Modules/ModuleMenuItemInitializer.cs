d7a74de194abf7bcd78151f2c51a80dd42aacd2a
Kooboo-youcai <youcai@kooboo.com>
2014-10-22 12:27:17 +0800

    add filter

--- Extensions/Kooboo.CMS.Toolkit/Modules/ModuleMenuItemInitializer.cs
+++ Extensions/Kooboo.CMS.Toolkit/Modules/ModuleMenuItemInitializer.cs
@@ -15,8 +15,18 @@ namespace Kooboo.CMS.Toolkit.Modules
         protected override bool GetIsActive(MenuItem menuItem, ControllerContext controllerContext)
         {
             string moduleRepositoryName = controllerContext.RequestContext.GetRequestValue("moduleRepositoryName");
+            string moduleRepositoryName2 = controllerContext.RequestContext.GetRequestValue("moduleRepositoryName2");
+
             string folderName = controllerContext.RequestContext.GetRequestValue("folderName");
-            return menuItem.RouteValues.GetString("moduleRepositoryName").Equals(moduleRepositoryName, StringComparison.OrdinalIgnoreCase) && menuItem.RouteValues.GetString("folderName").Equals(folderName, StringComparison.OrdinalIgnoreCase);
+
+            if (!String.IsNullOrWhiteSpace(moduleRepositoryName))
+            {
+                return menuItem.RouteValues.GetString("moduleRepositoryName").Equals(moduleRepositoryName, StringComparison.OrdinalIgnoreCase) && menuItem.RouteValues.GetString("folderName").Equals(folderName, StringComparison.OrdinalIgnoreCase);
+            }
+            else
+            {
+                return menuItem.RouteValues.GetString("moduleRepositoryName2").Equals(moduleRepositoryName2, StringComparison.OrdinalIgnoreCase) && menuItem.RouteValues.GetString("folderName").Equals(folderName, StringComparison.OrdinalIgnoreCase);
+            }
         }
     }
 }
